d27a501591aeb663bfc8d97db5f27d7ae36d7ee9
Evan Eckels <eveckels@gmail.com>
2017-11-02 18:08:22 -0500

    changes

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  var myWidth = $(window).width();
+  var myHeight = $(window).height();
+  var headerHeight = $('#header').height();
+  var centerHeight = $('#center').height() / -2 + 'px';
+
+  $('#center').css('margin-top', centerHeight);
+
+  if (myHeight > myWidth) {
+    $('.burbuja-wrapper').attr('id', 'burbuja-wrapper2');
+    $('.burbuja').attr('id', 'burbuja2');
+  }
+  if (myWidth > myHeight) {
+    $('.burbuja-wrapper').attr('id', 'burbuja-wrapper');
+    $('.burbuja').attr('id', 'burbuja');
+  }
+});
