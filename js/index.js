9bd13ce5798f7f2f74ad46aac4176c0fa43ed511
Evan Eckels <evan.eckels@yahoo.com>
2017-11-02 22:55:10 -0500

    updates

--- js/index.js
+++ js/index.js
@@ -1,17 +1,37 @@
 $(document).ready(function(){
-  var myWidth = $(window).width();
-  var myHeight = $(window).height();
-  var headerHeight = $('#header').height();
-  var centerHeight = $('#center').height() / -2 + 'px';
 
-  $('#center').css('margin-top', centerHeight);
+    var myWidth = $(window).width();
+    var myHeight = $(window).height();
+    var headerHeight = $('#header').height();
+    var centerHeight = $('#center').height() / -2 + 'px';
 
-  if (myHeight > myWidth) {
-    $('.burbuja-wrapper').attr('id', 'burbuja-wrapper2');
-    $('.burbuja').attr('id', 'burbuja2');
-  }
-  if (myWidth > myHeight) {
-    $('.burbuja-wrapper').attr('id', 'burbuja-wrapper');
-    $('.burbuja').attr('id', 'burbuja');
-  }
+    $('#center').css('margin-top', centerHeight);
+
+    if (myHeight > myWidth) {
+      $('.burbuja-wrapper').attr('id', 'burbuja-wrapper2');
+      $('.burbuja').attr('id', 'burbuja2');
+    }
+    if (myWidth > myHeight) {
+      $('.burbuja-wrapper').attr('id', 'burbuja-wrapper');
+      $('.burbuja').attr('id', 'burbuja');
+    }
+
+  $(window).resize(function() {
+    myWidth = $(window).width();
+    myHeight = $(window).height();
+    headerHeight = $('#header').height();
+    centerHeight = $('#center').height() / -2 + 'px';
+
+    $('#center').css('margin-top', centerHeight);
+
+    if (myHeight > myWidth) {
+      $('.burbuja-wrapper').attr('id', 'burbuja-wrapper2');
+      $('.burbuja').attr('id', 'burbuja2');
+    }
+    if (myWidth > myHeight) {
+      $('.burbuja-wrapper').attr('id', 'burbuja-wrapper');
+      $('.burbuja').attr('id', 'burbuja');
+    }
+  });
+
 });
