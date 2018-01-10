378032acfd631af53135a3634d7ecb48beda82da
Evan Eckels <eveckels@gmail.com>
2018-01-09 22:41:25 -0600

    revamped work area

--- js/index.js
+++ js/index.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
     var myWidth = $(window).width();
     var myHeight = $(window).height();
     var headerHeight = $('#header').height();
-    /*var centerHeight = $('#center').height() / -2 + 'px';
+    var centerHeight = $('#center').height() / -2 + 'px';
 
-    $('#center').css('margin-top', centerHeight);*/
-
+    $('#center').css('margin-top', centerHeight);
+    $('.smooth').smoothScroll();
     if (myHeight > myWidth) {
       $('.burbuja-wrapper').attr('id', 'burbuja-wrapper2');
       $('.burbuja').attr('id', 'burbuja2');
@@ -15,8 +15,6 @@ $(document).ready(function(){
       $('.burbuja-wrapper').attr('id', 'burbuja-wrapper');
       $('.burbuja').attr('id', 'burbuja');
     }
-
-    $('.smooth').smoothScroll();
 
   $(window).resize(function() {
     myWidth = $(window).width();
